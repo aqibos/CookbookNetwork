a072bc086026eb6c428eec4bde5539c2488ede36
eoyarzun <eoyarzun6668@gmail.com>
2015-04-08 18:06:38 -0400

    Add fail.php

---
+++ fail.php
@@ -0,0 +1,44 @@
+<?php
+session_start() ;
+?>
+<!DOCTYPE html>
+<html>
+	
+	<head>
+		 <meta charset="UTF-8">
+		<meta name="description" content="A virtual cookbook that allows user's to view, create and share recipes.">
+		<meta name="keywords" content="recipe, cookbook, food, ingredients">
+		<meta name="author" content="Cookbook Network Inc.">
+		<link rel="stylesheet" type="text/css" href="page_style.css">
+		<link href='http://fonts.googleapis.com/css?family=Tangerine:700' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=IM+Fell+Double+Pica' rel='stylesheet' type='text/css'>
+	</head>
+	
+	<body>
+		
+		<div class="background-image">
+		<div class="navigation-bar">
+			<table  class="navigation-bar-table">
+				<tr>
+					<td class="navigation-bar-table-left"><h1 class="navigation-bar-table-left-header">Cookbook Network</h1></td>
+					<td class="navigation-bar-table-right">
+						<ul class="upper-level-ul">
+						</ul>
+						
+					</td>
+				</tr>
+			</table>
+		</div>
+		</div>
+		<div class="content">
+			<h1>Oops! What you were trying to do didn't work.</h1>
+            <h2>Go back and try again!</h2>
+            <form>
+				<input type="submit" name="submit" value="Go BacK"> 
+			</form>
+		</div>
+		
+		<div class="footer"><p>&#169; Cookbook Network, 2015. All Rights Reserved.</p></div>
+		
+	</body>
+</html>
