1979b6cbf024fea202fa866c3bc7d2aa51cfc810
eoyarzun <eoyarzun6668@gmail.com>
2015-04-09 00:23:42 -0400

    Fix sql errors

--- login.php
+++ login.php
@@ -9,31 +9,29 @@ session_start() ;
 		<meta name="description" content="A virtual cookbook that allows user's to view, create and share recipes.">
 		<meta name="keywords" content="recipe, cookbook, food, ingredients">
 		<meta name="author" content="Cookbook Network Inc.">
-		<link rel="stylesheet" type="text/css" href="loginStyle.css">
+		<link rel="stylesheet" type="text/css" href="index_style-1.css">
 		<link href='http://fonts.googleapis.com/css?family=Tangerine:700' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=IM+Fell+Double+Pica' rel='stylesheet' type='text/css'>
 		
 		<script type="text/javascript">
 			var image1 = new Image()
-			image1.src = "Pizza-Food-Delicious-1440x2560.jpg"
+			image1.src = "images/Pizza-Food-Delicious-1440x2560.jpg"
 			var image2 = new Image()
-			image2.src = "delicious-pizza-food-1440x900.jpg"
+			image2.src = "images/delicious-pizza-food-1440x900.jpg"
 			var image3 = new Image()
-			image3.src = "food_spaghetti_1920x1080_wallp_2560x1440_miscellaneoushi.com_.jpg"
+			image3.src = "images/food_spaghetti_1920x1080_wallp_2560x1440_miscellaneoushi.com_.jpg"
 			var image4 = new Image()
-			image4.src = "Hot-and-Delicious-Food-Photos.jpg"
+			image4.src = "images/Hot-and-Delicious-Food-Photos.jpg"
 			var image5 = new Image()
-			image5.src = "Food-Delicious-Pizza-Olives-Olives-1440x2560.jpg"
+			image5.src = "images/Food-Delicious-Pizza-Olives-Olives-1440x2560.jpg"
 			var image6 = new Image()
-			image6.src = "loaf-delicious-cake-with-strawberries-wallpapers-1440x900.jpg"
+			image6.src = "images/loaf-delicious-cake-with-strawberries-wallpapers-1440x900.jpg"
 		</script>
 		
 	</head>
 	
 	<body>
-		
-        <?php include 'user_type.php' ?>
-		<p><img src="Pizza-Food-Delicious-1440x2560.jpg" name="slide" class="slideshow"/></p>
+		<p><img src="images/Pizza-Food-Delicious-1440x2560.jpg" name="slide" class="slideshow"/></p>
 		
 		<div class="full-body">
 			
@@ -42,7 +40,7 @@ session_start() ;
             <?php
                 // define variables and set to empty values
                 $emailERR = $passwordERR = "" ;
-                $user_email = $user_password = "";
+                $email = $user_password = "";
                 
                 if ($_SERVER["REQUEST_METHOD"] == "POST"){
                     if(empty($_POST["email"])){
@@ -62,9 +60,9 @@ session_start() ;
                     }
                     
                     $valid = validate_user($user_email, $user_password) ;
+					
                     if($valid == true) {
-                        $emailERR = "valid credentials" ;
-                        header('Location: own_recipe.php') ;
+                        header('Location: home-page-registered.php') ;
                     } else {
                         $emailERR = "Invalid email/password" ;
                     }
@@ -80,30 +78,30 @@ session_start() ;
                 
                 function validate_user($email , $password)
                 {
-                    $servername = 'localhost' ;
-                    $username = 'root';
-                    $pw = '';
-                    $dbname = 'cookbooknetwork' ;
+                    $servername="localhost" ;
+					$username = "root" ;
+					$pw = "" ;	
+					$dbname = "cookbooknetwork" ;
 
-                    $conn = new mysqli($servername, $username, $pw, $dbname) or die("Unable to connect");
-                    
-                    $sql = "SELECT * FROM  MyUsers WHERE email = '$email' AND password = '$password'" ;
+					$conn = new mysqli($servername, $username, $pw, $dbname);
+
+					if ($conn -> connect_error)
+						die("Connection failed: ".$conn -> connect_error);
+                    $sql = "SELECT * FROM  Account WHERE email = '$email' AND password = '$password'" ;
                     
                     $result = $conn -> query($sql) ;
                     
-                    if($result -> num_rows > 0) {
+                    if($result->num_rows > 0) {
                         $row = $result -> fetch_assoc() ;
-                        $_SESSION["userID"] = $row["userID"] ;
+                        $_SESSION["loggedin"] = true ;
                         $_SESSION["username"] = $row["username"] ;
-                        
-                        if($row["isAdmin"] === 1){
-                            $_SESSION["userType"] = ADMIN ;
-                        } else {
-                            $_SESSION["userType"] = REGISTERED ;
-                        }
+                        $_SESSION["isAdmin"] = $row["isAdmin"] ;
+            
                         $conn -> close() ;
                         return true ;
                     } else {
+                        $_SESSION["loggedin"] = false ;
+					
                         $conn -> close() ;
                         return false ;
                     }    
