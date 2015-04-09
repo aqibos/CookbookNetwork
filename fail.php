43055e7121e210075f4ecd49b2c6c052a2446b60
eoyarzun <eoyarzun6668@gmail.com>
2015-04-09 13:03:15 -0400

    Enable 'Go Back' button

--- fail.php
+++ fail.php
@@ -5,7 +5,7 @@ session_start() ;
 <html>
 	
 	<head>
-		 <meta charset="UTF-8">
+		<meta charset="UTF-8">
 		<meta name="description" content="A virtual cookbook that allows user's to view, create and share recipes.">
 		<meta name="keywords" content="recipe, cookbook, food, ingredients">
 		<meta name="author" content="Cookbook Network Inc.">
@@ -15,30 +15,26 @@ session_start() ;
 	</head>
 	
 	<body>
+		<div class="background-image"></div>
 		
-		<div class="background-image">
 		<div class="navigation-bar">
-			<table  class="navigation-bar-table">
-				<tr>
-					<td class="navigation-bar-table-left"><h1 class="navigation-bar-table-left-header">Cookbook Network</h1></td>
-					<td class="navigation-bar-table-right">
-						<ul class="upper-level-ul">
-						</ul>
-						
-					</td>
-				</tr>
-			</table>
+			<?php include 'check-menu.php' ?>
 		</div>
-		</div>
+		
 		<div class="content">
 			<h1>Oops! What you were trying to do didn't work.</h1>
             <h2>Go back and try again!</h2>
             <form>
-				<input type="submit" name="submit" value="Go BacK"> 
+				<button onclick="goBack()">Go Back</button>
 			</form>
 		</div>
 		
 		<div class="footer"><p>&#169; Cookbook Network, 2015. All Rights Reserved.</p></div>
-		
+		<script type="text/javascript">
+			function goBack()
+			{
+				window.history.back();
+			}
+		</script>
 	</body>
 </html>
