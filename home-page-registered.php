07f3eae299e4e03506d10c39b2757723d83d5e8e
eoyarzun <eoyarzun6668@gmail.com>
2015-04-09 00:21:53 -0400

    Add check-menu.php usage

---
+++ home-page-registered.php
@@ -0,0 +1,39 @@
+<?php
+session_start() ;
+?>
+<!DOCTYPE html>
+<html>
+	
+	<head>
+		<meta charset="UTF-8">
+		<meta name="description" content="A virtual cookbook that allows user's to view, create and share recipes.">
+		<meta name="keywords" content="recipe, cookbook, food, ingredients">
+		<meta name="author" content="Cookbook Network Inc.">
+		<link rel="stylesheet" type="text/css" href="page_style.css">
+		<link href='http://fonts.googleapis.com/css?family=Tangerine:700' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=IM+Fell+Double+Pica' rel='stylesheet' type='text/css'>
+	</head>
+	
+	<body>
+		
+		<div class="background-image"></div>
+		
+		<div class="navigation-bar">
+			<?php include 'check-menu.php' ?>
+		</div>
+		
+		<div class="content">
+			<h1>Welcome, <?php echo $_SESSION["username"] ?></h1>
+			
+			<div class="content-div">
+				<a href=""><div class="content-div-row1">Account Info</div></a>
+				<a href=""><div class="content-div-row1">MyRecipes</div></a>
+				<a href=""><div class="content-div-row1">MyCookbooks</div></a>
+			</div>
+
+		</div>
+		
+		<div class="footer"><p>&#169; Cookbook Network, 2015. All Rights Reserved.</p></div>
+		
+	</body>
+</html>
