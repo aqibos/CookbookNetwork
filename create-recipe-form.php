d2beae16564aba82df96df04827d3c893f88c282
aashah7 <ashah023@fiu.edu>
2015-04-10 18:17:21 -0400

    updates to create recipe and edit recipe

---
+++ create-recipe-form.php
@@ -0,0 +1,328 @@
+<?php
+            
+    //connect to db
+    function connectToDb($servername, $username, $password, $dbname)
+    {
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Check connection
+        if ($conn->connect_error) 
+        {
+            echo '<script type="text/javascript">'
+                . 'alert("Sorry, could not connect to CookbookNetwork database.");'
+                . '</script>';
+            die("Connection failed: " . $conn->connect_error);
+        }
+        
+        return $conn;
+    }
+
+    //check privacy
+    function checkPrivacy($conn)
+    {
+        //privacy
+        $privacy = ($_POST['privacy-setting']);
+        $allValidFriends = TRUE;
+        $invalidFriend = "";
+
+        //check all friends
+        if ($privacy == "friendly")
+        {
+            $validFriend = FALSE;
+            for ($x = 0; ;$x++)
+            {
+                if (!array_key_exists("friendName" . $x, $_POST))
+                {
+                     break;
+                }
+
+                if (!empty($_POST["friendName" . $x]))
+                {
+                    $currFriend = $_POST["friendName" . $x];
+
+                    //check if currFriend is a registered user
+                    $sql = "SELECT email FROM Account";
+                    $result = mysqli_query($conn, $sql);
+
+                    if (mysqli_num_rows($result) > 0) {
+                        
+                        //get all emails
+                        while($row = mysqli_fetch_assoc($result)) 
+                        {
+                            $currEmail = $row["email"];
+                            if ($currFriend == $currEmail) 
+                            {
+                                $validFriend = TRUE;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (!$validFriend)
+                    {
+                        $invalidFriend = $currFriend;
+                        return FALSE;
+                    }
+                }
+            }
+        }
+
+        return TRUE;
+    }
+
+    //get temp name given by php
+    function getImageTmpName()
+    {
+        return $_FILES['photo']['tmp_name'];
+    }
+
+    //get file name given by owner
+    function getImageFullName()
+    {
+        return $_FILES['photo']['name'];
+    }
+    
+    //check if file was uploaded
+    function checkImageUploaded()
+    {
+        if ($_FILES['photo']['size'] == 0) {
+            return FALSE;
+        }
+        return TRUE;
+    }
+
+    //return the path on the server of img
+    function getImagePath($recipeId)
+    {
+        return $recipeId . '/' . $_FILES['photo']['name'];
+    }
+
+    //update the img path in db
+    function updateImagePathInDB($conn, $path, $recipeId)
+    {
+        $sql = "UPDATE Recipe 
+                SET img_path='$path' 
+                WHERE recipe_id='$recipeId';";
+        
+        if (!($conn->query($sql) === TRUE))
+        {
+            echo "$conn->error";
+            cleanDbTables($recipeId, $conn);
+            return FALSE;
+        }
+        
+        return TRUE;
+    }
+
+    //get recipe name
+    function getRecipeName()
+    {
+        return ($_POST['recipe-name']);
+    }
+
+    //get recipe photo
+    function getPicture()
+    {
+        $picture = ($_FILES['photo']['tmp_name']);
+    }
+
+    //get all steps of recipe
+    function getAllSteps()
+    {
+        $allSteps = "";
+            for ($x = 1; ;$x++)
+            {
+                if (!array_key_exists("step" . $x, $_POST))
+                {
+                     break;
+                }
+
+                if (!empty($_POST["step" . $x]))
+                {
+                    $allSteps = $allSteps . ($_POST["step" . $x]) . ", ";
+                }
+            }
+
+            if (strlen($allSteps) > 0)
+            {
+                //remove extra comma and space
+                $allSteps = substr($allSteps, 0, strlen($allSteps) - 2);
+            }
+
+            return $allSteps;
+    }
+
+    //get privacy
+    function getPrivacy()
+    {
+        return ($_POST['privacy-setting']);
+    }
+
+    //insert recipe into db
+    function insertRecipeIntoDB($recipeName, $allSteps, $privacy, $conn)
+    {
+            //add recipe into db
+            $sql = "INSERT INTO Recipe (recipe_title, directions, rating, times_rated, visibility) 
+                VALUES ( '$recipeName', '$allSteps', 0.0, 0, '$privacy')";
+
+            if ($conn->query($sql) === TRUE)
+            {
+                //get recipe id
+                return mysqli_insert_id($conn);
+            }
+            else
+            {
+                //return a value to acknowledge failure
+                return -1;
+            }
+    }
+    
+    //count number of friend inputs
+    function countFriends()
+    {
+        $numFriends = 0;
+        for ($x = 0; ;$x++)
+        {
+            if (!array_key_exists("friendName" . $x, $_POST))
+            {
+                 break;
+            }
+
+            if (!empty($_POST["friendName" . $x]))
+            {
+                $numFriends++;
+            }
+        }
+        return $numFriends;
+    }
+    
+
+    //add friends to db
+    function addFriendsToDB($conn, $numFriends, $lastId)
+    {
+        for ($x = 0; $x < $numFriends; $x++)
+        {
+            $currFriend = $_POST["friendName" . $x];
+
+            $sql = "INSERT INTO Friends (recipe_id, email) 
+                    VALUES ( '$lastId', '$currFriend')";
+
+                if (!($conn->query($sql) === TRUE)) {
+                    cleanDbTables($lastId, $conn);
+                    
+                    return false;
+                }
+        }
+        return true;
+    }
+
+    //add ingredients to db
+    function addIngredientsToDB($conn, $lastId)
+    {
+        for ($x = 1; ; $x++) 
+        {
+            if (!array_key_exists("ingredient" . $x, $_POST))
+            {
+                 break;
+            }
+
+            if (!empty($_POST["ingredient" . $x]))
+            {
+                $currIngredient = $_POST["ingredient" . $x];
+                $sql = "INSERT INTO Ingredient (name, recipe_id) 
+                    VALUES ( '$currIngredient', '$lastId')";
+
+                if (!($conn->query($sql) === TRUE)) {
+                    cleanDbTables($lastId, $conn);
+                    return false;
+                }   
+            }
+        } 
+        return true;
+    }
+
+    //add tags to db
+    function addTagsToDB($conn, $lastId)
+    {
+        //tags
+        $tags = [
+            0 => "american",
+            1 => "gluten-free",
+            2 => "beef",
+            3 => "appetizer",
+            4 => "asian",
+            5 => "paleo",
+            6 => "chicken",
+            7 => "beverages",
+            8 => "greek",
+            9 => "vegan",
+            10 => "pork",
+            11 => "breakfast & brunch",
+            12 => "italian",
+            13 => "vegetarian",
+            14 => "poultry",
+            15 => "desserts",
+            16 => "jamaican",
+            17 => "seafood",
+            18 => "lunch",
+            19 => "latin",
+            20 => "salad",
+            21 => "desi",
+            22 => "soup",
+        ];
+        
+        //for each tag, check if selected
+        for ($x = 0; $x < count($tags); $x++)
+        {
+            if (isset($_POST[$tags[$x]]))
+            {   
+                $tagName = $tags[$x];
+                $sql = "INSERT INTO Tag (name, type, type_id) 
+                    VALUES ( '$tagName', 'RECIPE', '$lastId')";
+
+                if (!($conn->query($sql) === TRUE)) {
+                    cleanDbTables($lastId, $conn);
+                    return false;
+                }   
+
+            }
+        }
+        return true;
+    }
+
+    //navigate to view recipe
+    function redirectToViewRecipe($lastId)
+    {
+        header('Location: view-recipe.php?recipe_id=' . $lastId);
+    }
+
+    //close connection
+    function closeDBConnection($conn)
+    {
+        $conn->close();
+    }
+
+    //in case of error, remove extranneous data from db
+    function cleanDbTables($lastId, $conn)
+    {
+        removeFromDb("Friends", "recipe_id", $lastId, $conn);
+        removeFromDb("Tag", "type_id", $lastId, $conn);
+        removeFromDb("Recipe", "recipe_id", $lastId, $conn);
+        removeFromDb("Ingredient", "recipe_id", $lastId, $conn);
+    }
+
+    //utility function to clean db
+    function removeFromDb($tableName, $attributeName, $lastId, $conn)
+    {
+
+        $sql = "DELETE FROM " . $tableName . 
+                " WHERE " . $attributeName . "=" . $lastId . ";";
+
+        if (!($conn->query($sql) === TRUE)) 
+        {
+            //keep trying until success
+            removeFromDb($tableName, $attributeName, $lastId, $conn);
+        }
+    }
+
+?>
---
+++ createRecipeFormHandler.js
@@ -0,0 +1,219 @@
+var ingredientIdNum = 5;             //no. of ingredients on page + 1
+var stepIdNum = 5;                   //no. of steps on page + 1
+var friends = 0;                     //no. of friends
+var isFriendly = false;
+
+//adds ingredient input to page
+function addIngredientField() {
+    var allIngredients = document.getElementsByClassName("ingredient");
+    var lastIngredient = allIngredients[allIngredients.length - 1];
+    var clone = lastIngredient.cloneNode(true);
+    clone.id= "ingredient" + ingredientIdNum;
+    lastIngredient.parentNode.appendChild(clone);
+    fixIngredientInputName();
+}
+
+//fixes the last added ingredient's name
+function fixIngredientInputName()
+{
+    var allIngredientInputs = document.getElementsByClassName("ingredientInput");
+    var lastIngredientInput = allIngredientInputs[allIngredientInputs.length - 1];
+    lastIngredientInput.name = "ingredient" + ingredientIdNum++;
+}
+
+//adds step input on page
+function addStepField() {
+    var allSteps = document.getElementsByClassName("step");
+    var lastStep = allSteps[allSteps.length - 1];
+    var clone = lastStep.cloneNode(true);
+    clone.id= "step" + stepIdNum;
+    lastStep.parentNode.appendChild(clone);
+    fixStepInputName();
+}
+
+//fixes the last added step's name
+function fixStepInputName()
+{
+    var allStepInputs = document.getElementsByClassName("stepInput");
+    var lastStepInput = allStepInputs[allStepInputs.length - 1];
+    lastStepInput.name = "step" + stepIdNum++;
+}
+
+//check if the privacy is set to friendly and add input fields
+function checkFriendly()
+{
+    var privacyDropdown = document.getElementById("privacy");
+    var selectedString = privacyDropdown.options[privacyDropdown.selectedIndex].value;
+
+    if (selectedString == "friendly")
+    {
+        isFriendly = true;
+        var privacyInput = document.getElementById("privacy-input");
+        privacyInput.insertAdjacentHTML( 'beforeBegin', '<p id="friendText">Enter the email address of your friends:</p>' );
+        addFriendInput();
+        document.getElementById("addFriend").style.visibility='visible';
+    }
+    else
+    {
+        removeInputs();
+        friends = 0;
+        document.getElementById("addFriend").style.visibility='hidden';
+        var friendText = document.getElementById("friendText");
+        if (friendText != null)
+        {
+            var parent = friendText.parentNode;
+            parent.removeChild(friendText);
+        }
+        isFriendly = false;
+
+    }
+}
+
+//add more friend input on page
+function addFriendInput()
+{
+    var newTextBox = document.createElement("input");
+    newTextBox.setAttribute("type", "text");
+    newTextBox.setAttribute("name", "friendName" + friends);
+    newTextBox.setAttribute("placeholder", "Enter Friend Email Here");
+    newTextBox.setAttribute("id", "friendName" + friends++);
+    newTextBox.style.width = '100%';
+    newTextBox.style.display = 'block';
+    var containerDiv = document.getElementById("privacy-input");
+    containerDiv.appendChild(newTextBox);
+}
+
+//remove all friend input on page
+function removeInputs()
+{
+    var i;
+    for (i = 0; i < friends; i++)
+    {
+        var currFriendInput = document.getElementById("friendName" + i);
+        var parent = currFriendInput.parentNode;
+        parent.removeChild(currFriendInput);
+    }
+}
+
+//validate form before submission
+function validateForm()
+{
+    if (checkNameInput()
+        && checkIngredientInputs()
+        && checkStepInputs()
+        && checkFriendInputs()
+        && confirmSubmission())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+//recipe name can not be empty
+function checkNameInput()
+{
+    var recipeName = document.getElementById("recipe-name").value;
+    if (recipeName.trim() === "")
+    {
+        alert("Recipe name can not be empty!");
+        return false;
+    }
+    return true;
+}
+
+
+//must have at least one ingredient 
+function checkIngredientInputs()
+{
+    var i;
+    var emptyIngredients = true;
+    for (i = 1; i < ingredientIdNum; i++)
+    {
+        var currIngredient = document.getElementById("ingredient" + i).value;
+        if (currIngredient.trim() != "")
+        {
+            emptyIngredients = false;
+        }
+    }
+
+    if (emptyIngredients)
+    {
+        alert("Recipe must contain at least one ingredient.");
+        return false;
+    }
+
+    return true;
+}
+
+//must have at least one step
+function checkStepInputs()
+{
+    var emptySteps = true;
+    for (i = 1; i < stepIdNum; i++)
+    {
+        var currStep = document.getElementById("step" + i).value;
+        if (currStep.trim() != "")
+        {
+            emptySteps = false;
+        }
+    }
+
+    if (emptySteps)
+    {
+        alert("Recipe must contain at least one step.");
+        return false;
+    }
+    return true;
+}
+
+//must have at least one friend email address if 'friendly'
+function checkFriendInputs()
+{
+    var emptyFriends = true;
+    if (isFriendly)
+    {
+        var i;
+        for (i = 0; i < friends; i++)
+        {
+            var currFriend = document.getElementById("friendName" + i).value;
+            //check if empty
+            if (currFriend.trim() != "")
+            {
+                emptyFriends = false;
+
+                //check if valid format
+                var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+                if (!re.test(currFriend))
+                {
+                    alert("'" + currFriend + "' is not a valid email address.");
+                    return false;
+                }
+            }
+        }
+
+        if (emptyFriends)
+        {
+            alert("You must specify at least one friend for 'FRIENDLY' privacy." +
+                  "\nYou can use 'PRIVATE' privacy, if you want the recipe hidden." +
+                  "\nYou can change this later using 'Edit Recipe'.");
+            return false;
+        }
+    }
+    return true;
+}
+
+function confirmSubmission()
+{
+    var recipeName = document.getElementById("recipe-name").value;
+    if (confirm("Are you sure you want to create recipe '" + recipeName + "'?"))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
