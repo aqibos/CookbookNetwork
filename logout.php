5770acad75e5e7c31edcc087a11bfc1c97823713
Anais Hernandez <ahern379@fiu.edu>
2015-04-15 13:18:22 -0400

    fix logout again

--- logout.php
+++ logout.php
@@ -1,4 +1,4 @@
 <?php session_start();
-    $id = $session['User_id'];//user have user_id attribute
     unset($_SESSION);
-    header("Location:index-1.html"); ?>
+    header("Location:index-1.html"); 
+?>
