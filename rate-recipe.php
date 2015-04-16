c909f0dfe5bf83f8a1f1e63dbb220fd9a27e7cc0
Anais Hernandez <ahern379@fiu.edu>
2015-04-16 17:10:31 -0400

    rate recipe, bad version

--- rate-recipe.php
+++ rate-recipe.php
@@ -1,67 +1,10 @@
 <?php
     session_start();
-    $recipe_id = 1;
-    $pwerror="";
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST")
-    {
-        $host="localhost";              // Host name 
-        $username="root";               // Mysql username 
-        $password="";                   // Mysql password 
-        $db_name="cookbooknetwork";     // Database name 
-        $tbl_name="Recipe";             // Table name 
-
-        // Connect to server and select databse.
-        $link = new mysqli($host, $username, $password, $db_name);
-        if ($link -> connect_error)
-            die("Connection failed: ".$link -> connect_error);
-
-        // current and new password sent from form
-        $ratestar = $_POST['rating'];
-
-        $sql="SELECT * FROM $tbl_name WHERE recipe_id= '$recipe_id'";
-        $result = $link -> query($sql);
-        $count = $result->num_rows;
-
-        if($count == 1)     //input of current password is incorrect
-        {
-            $newrating; 
-            $row = $result->fetch_assoc();
-            $currentrating = $row['rating'];    //get current rating
-            $totalrate = $row['times_rated'];   //get total amount of rating
-            
-            if($ratestar == "star1")
-                $newrating = 1; 
-            else if($ratestar == "star2")
-                $newrating = 2; 
-            if($ratestar == "star3")
-                $newrating = 3; 
-            if($ratestar == "star4")
-                $newrating = 4; 
-            else    //star5
-                $newrating = 5; 
-            
-            $totalrate++;
-            $newrating = ($currentrating + $newrating) / $totalrate ;
-            
-            $sql = "UPDATE $tbl_name SET times_rated ='$totalrate', rating = '$newrating' WHERE `recipe_id`='$recipe_id'";
-            if ($link->query($sql) != true)     //unsuccessful query
-            {
-                $pwerror= "ERROR: Could not able to execute $sql. " . $link->connect_error;
-            } 
-            redirect();
-        }
-        else
-        {
-            
-        }
-    }
-
-    function redirect()
-    {
-        header('Location: recipe.php');
-    }
+    //get recipe_id to be rated
+    $recipeID = $_GET['recipe_id'];
 ?>
+
 <!DOCTYPE html>
 <html>
 
@@ -76,7 +19,25 @@
 	</head>
 	
 	<body>
-		
+		<?php 
+            $pwerror="";
+            $host="localhost";              // Host name 
+            $username="root";               // Mysql username 
+            $password="";                   // Mysql password 
+            $db_name="cookbooknetwork";     // Database name 
+            $tbl_name="Recipe";             // Table name 
+
+            // Connect to server and select databse.
+            $link = new mysqli($host, $username, $password, $db_name);
+            if ($link -> connect_error)
+                die("Connection failed: " . $link -> connect_error);
+
+            $sql="SELECT recipe_title FROM $tbl_name WHERE recipe_id= '$recipeID'";
+            $result = $link -> query($sql);
+            $row = $result->fetch_assoc();
+            $title = $row['recipe_title'];
+            mysqli_close($link);  
+        ?>
 		<div class="background-image"></div>
 
 		<div class="navigation-bar">
@@ -86,17 +47,39 @@
 		<div class="content">
 			<h1 class="center">Rate Recipe</h1>
 			<div class="center">
-				<p>We would love to hear your feedback on "Yum, Yum Pizza"!</p>
-				<form name="rate" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+				<p>We would love to hear your feedback on <?php echo $title; ?>!</p>
+                <p><?php echo $pwerror; ?></p>
+				<form name="rate" method="POST" onsubmit="return validate()" action="rate-recipe-form.php">
 					<input type="radio" name="rating" value="star1">&nbsp;&nbsp;<img src="images/star1.png"><br/><br/>
 					<input type="radio" name="rating" value="star2">&nbsp;&nbsp;<img src="images/star2.png"><br/><br/>
 					<input type="radio" name="rating" value="star3">&nbsp;&nbsp;<img src="images/star3.png"><br/><br/>
 					<input type="radio" name="rating" value="star4">&nbsp;&nbsp;<img src="images/star4.png"><br/><br/>
 					<input type="radio" name="rating" value="star5">&nbsp;&nbsp;<img src="images/star5.png"><br/><br/>
-					<input type="submit" value="Rate!"> <input type="submit" value="Cancel">
+				    <input type="submit" value="Cancel" onclick="window.history.back(); return false;">
+                    <input type="submit" value="Rate!" />
 				</form>
 			</div>
 		</div>
+        
+        <script type="text/javascript">
+            
+            function validate () 
+            {
+                var checkboxes = document.getElementsByName('rating');
+                var i = checkboxes.length - 1;
+
+                for ( ; i >= 0 ; i-- ) 
+                {
+                    if ( checkboxes[i].checked )   
+                        return true;
+                }
+
+                alert("Must choose a rating.");
+                return false;
+            }
+            
+        </script>
+                
 		
 		<div class="footer"><p>&#169; Cookbook Network, 2015. All Rights Reserved.</p></div>
 		
