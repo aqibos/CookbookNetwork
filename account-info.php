8bf301fc81ff808d1628a49531abb6e06402d8d6
aashah7 <ashah023@fiu.edu>
2015-04-17 21:11:52 -0400

    fix account-info.php

--- account-info.php
+++ account-info.php
@@ -1,6 +1,62 @@
 <?php
 	session_start();
-	$username = $_SESSION["username"];
+	$user = $_SESSION["username"];
+
+    $fromNav = TRUE;
+
+    /*if (isset($_GET["user_id"]))
+    {
+        $fromNav = FALSE;
+        echo "From nav? FALSE";
+    }
+    else
+    {
+        echo "From nav?  TRUE";
+    }*/
+    
+
+    include 'create-recipe-form.php';
+
+    //credentials
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "cookbooknetwork";
+
+    //connect to db
+    $conn = connectToDb($servername, $username, $password, $dbname);
+
+    //get user id
+    $userId = getAuthorId($conn, $user);
+
+    //get email
+    $sql = "SELECT email
+                FROM Account
+                WHERE user_id = '$userId'";
+        
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $userEmail = $row["email"];
+
+    //check status
+    $sql = "SELECT isAdmin
+                FROM Account
+                WHERE user_id = '$userId'";
+        
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $isAdmin = $row["isAdmin"];
+    $status = "";
+
+    if ($isAdmin == 0)
+    {
+        $status = "Registered";
+    }
+    else
+    {
+        $status = "Administrator";
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -18,7 +74,7 @@
 	
 	<body>
 		
-		<div class="background-image"></div>
+		<img class="background-image" src="images/food_spaghetti_1920x1080_wallp_2560x1440_miscellaneoushi.com_.jpg" height="700"/>
 		
 		<div class="navigation-bar">
 			<?php include 'check-menu.php'; ?>
@@ -30,23 +86,24 @@
 			<table class="content-table">
 				<tr class="content-table-row">
 					<td class="content-table-left"><h3>User Name:</h3></td>
-					<td class="content-table-right">JohnDoe</td>
+					<td class="content-table-right"><?php echo $user; ?></td>
 				</tr>
 				<tr class="content-table-row">
 					<td class="content-table-left"><h3>E-mail:</h3></td>
-					<td class="content-table-right">johndoe@gmail.com</td>
+					<td class="content-table-right"><?php echo $userEmail; ?></td>
 				</tr>
 				<tr class="content-table-row">
 					<td class="content-table-left"><h3>Status:</h3></td>
-					<td class="content-table-right">Registered</td>
+					<td class="content-table-right"><?php echo $status; ?></td>
 				</tr>
-				<tr class="content-table-row">
-					<td class="content-table-left"><h3>Date Joined:</h3></td>
-					<td class="content-table-right">03/30/2015</td>
-				</tr>
-				<tr style="text-align:right">
-					<td style="text-align:right"><a href="change-pw.php"><div class="button">Change password?</div></a></td>
-					<td style="text-align:right"><a href="delete-user.php"><div class="button">Delete User</div></a></td>
+				<tr >
+                    <td></td>
+					<td >
+                        <div class="wrapper-button">
+                            <a href="change-pw.php"><div class="button">Change password</div></a>
+                        </div>
+                    </td>
+					<!--<td style="text-align:right"><a href="delete-user.php?user_id=<?php echo $userId; ?>"><div class="button">Delete User</div></a></td>-->
 				</tr>
 			</table>
 			
