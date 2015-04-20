8299bc73ac398096ca71e1ab347d7619f770a2d2
eoyarzun <eoyarzun6668@gmail.com>
2015-04-20 13:58:33 -0400

    Add my-recipes.php

---
+++ my-recipes.php
@@ -0,0 +1,42 @@
+<?php
+session_start() ;
+include 'recipe-search-handler.php' ;
+
+function printMyRecipes()
+{
+	$conn = getConn() ;
+	$userid = $_SESSION["userid"] ;
+	
+	$sql = " SELECT * FROM recipe
+				WHERE author = '$userid'" ;
+	$result = $conn -> query($sql) ;
+	printResult($result) ;
+}
+?>
+<!DOCTYPE html>
+<html>
+	
+	<head>
+		 <meta charset="UTF-8">
+		<meta name="description" content="A virtual cookbook that allows user's to view, create and share recipes.">
+		<meta name="keywords" content="recipe, cookbook, food, ingredients">
+		<meta name="author" content="Cookbook Network Inc.">
+		<link rel="stylesheet" type="text/css" href="page_style.css">
+		<link href='http://fonts.googleapis.com/css?family=Tangerine:700' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=IM+Fell+Double+Pica' rel='stylesheet' type='text/css'>
+		<title>MyRecipes </title>
+	</head>
+	
+	<body>
+		<img class="background-image" src="images/delicious-pizza-food-1440x900.jpg" height="700"/>
+		<div class="navigation-bar">
+			<?php include 'check-menu.php'?>
+		</div>
+		
+		<div class="content">
+		<?php printMyRecipes();?>
+		</div>
+		
+		<div class="footer"><p>&#169; Cookbook Network, 2015. All Rights Reserved.</p></div>
+	</body>
+</html>
