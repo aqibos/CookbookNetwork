9455ea6ce608eaca592caf25d2164a50ea32076a
aashah7 <ashah023@fiu.edu>
2015-04-20 19:59:40 -0400

    complete flag.php

--- flag.php
+++ flag.php
@@ -1,5 +1,67 @@
 <?php
 session_start() ;
+
+/*
+flag_id INT(7) UNSIGNED AUTO_INCREMENT,
+recipe_id INT(7) UNSIGNED NOT NULL,
+reason varchar(50) NOT NULL,
+comment TEXT,
+user_id INT(6) UNSIGNED NOT NULL,
+*/
+
+include 'edit-recipe-form-handler.php';
+include 'create-recipe-form.php';
+
+//check if logged in 
+if (!isset($_GET["recipe_id"]) || !$_SESSION['loggedin'])
+{
+    header('Location: fail.php');
+}
+
+//use db credentials
+include 'db-credentials.php';
+
+//get user id
+$userId = $_SESSION['userid'];
+
+//connect to db
+$conn = connectToDb($servername, $username, $password, $dbname);
+
+//get recipe id
+$recipeId = $_GET['recipe_id'];
+
+//get recipe name
+$recipeName = getRecipeNameFromDB($conn, $recipeId);
+
+//if invalid recipe id
+if ($recipeName == '')
+{
+    header('Location: fail.php');
+}
+
+//check if submit form
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    //get reason - dropdown
+    $reason = $_POST['reason'];
+    
+    //get comments
+    $comment = $_POST['comment'];
+
+    //insert into db
+    $sql = "INSERT INTO Flag (recipe_id, reason, comment, user_id) 
+            VALUES ( '$recipeId', '$reason', '$comment', '$userId')";
+    
+    if (!($conn->query($sql) === TRUE)) 
+    {
+        //echo "$conn->error";
+        header('Location: fail.php');
+    }
+
+    //redirect to view recipe
+    header('Location: view-recipe.php?recipe_id=' . $recipeId);
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,32 +78,17 @@ session_start() ;
 	
 	<body>
 		
-		<div class="background-image"></div>
+		<img class="background-image" src="images/food_spaghetti_1920x1080_wallp_2560x1440_miscellaneoushi.com_.jpg" height="700"/>
 		
-		<div class="navigation-bar">
-			<table  class="navigation-bar-table">
-				<tr>
-					<td class="navigation-bar-table-left"><h1 class="navigation-bar-table-left-header"><a href="index.php">Cookbook Network</a></h1></td>
-					<td class="navigation-bar-table-right">
-						<ul class="upper-level-ul">
-							<!--  
-								<li> <?php echo $_SESSION["username"]; ?>
-							-->
-							<?php include 'registered_nav.php'?>
-							
-						</ul>
-						
-					</td>
-				</tr>
-			</table>
+		<div class="navigation-bar">				   
+			<?php include 'check-menu.php'; ?>
 		</div>
 		
 		<div class="content">
 			<h1>Flag Form</h1>
-			<h2>Recipe: Yum, Yum Pizza!</h2>
-			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+			<h2>Recipe: <?php echo $recipeName; ?></h2>
+			<form method="post"> 
 				<select name="reason">
-					<option value="select" selected>-Reason-</option>
 			  		<option value="language">Inappropriate Language</option>
 			  		<option value="tag">Incorrect Tag</option>
 			  		<option value="ingredients">Incorrect Ingredients</option>
