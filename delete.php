a6f28d499a47f78add022ca8cfd8c0206fd21469
eoyarzun <eoyarzun6668@gmail.com>
2015-04-20 23:55:09 -0400

    Add delete.php

--- delete.php
+++ delete.php
@@ -1,6 +1,7 @@
 <?php
 session_start() ;
 include 'db-credentials.php' ;
+
 if(isset($_GET["recipe_id"]))
 	deleteRecipe() ;
 else 
@@ -12,34 +13,24 @@ function deleteRecipe()
 	
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	
-	echo '<script>confirm(\'assad\'); </script>'; 
-
 	$sql = " DELETE FROM recipe 
 				WHERE recipe_id = '$recipe_id'" ;
 	
-	if ($conn->query($sql) !== TRUE) 
-	{		
-		header('Location:fail.php') ;
-	}
-	
-	
+	if ($conn->query($sql) !== TRUE) header('Location:fail.php') ;
+
 	$sql = " DELETE FROM tag 
 				WHERE type = 'RECIPE'
 					AND type_id = '$recipe_id'" ;
 
-	if ($conn->query($sql) !== TRUE) 
-	{		
-		header('Location:fail.php') ;
-	}	
+	if ($conn->query($sql) !== TRUE) header('Location:fail.php') ;
+	
 	
 	$sql = " DELETE FROM friends 
 				WHERE type = 'RECIPE'
 					AND type_id = '$recipe_id'" ;
 
-	if ($conn->query($sql) !== TRUE) 
-	{		
-		header('Location:fail.php') ;
-	}
+	if ($conn->query($sql) !== TRUE) header('Location:fail.php') ;
+	
 	
 	$conn->close();
 	header('Location:my-recipes.php') ;
@@ -50,31 +41,23 @@ function deleteCookbook()
 	$cookbook_id = $_GET["cookbook_id"] ;
 	
 	$conn = new mysqli($servername, $username, $password, $dbname);
+	
 	$sql = " DELETE FROM cookbook 
 				WHERE cookbook_id = '$cookbook_id'" ;
 
-	if ($conn->query($sql) !== TRUE) 
-	{		
-		header('Location:fail.php') ;
-	}
+	if ($conn->query($sql) !== TRUE) header('Location:fail.php') ;
 	
 	$sql = " DELETE FROM tag 
 				WHERE type = 'COOKBOOK'
 					AND type_id = '$cookbook_id'" ;
 
-	if ($conn->query($sql) !== TRUE) 
-	{		
-		header('Location:fail.php') ;
-	}	
+	if ($conn->query($sql) !== TRUE) header('Location:fail.php') ;
 	
 	$sql = " DELETE FROM friends 
 				WHERE type = 'COOKBOOK'
 					AND type_id = '$cookbook_id'" ;
 
-	if ($conn->query($sql) !== TRUE) 
-	{		
-		header('Location:fail.php') ;
-	}
+	if ($conn->query($sql) !== TRUE)  header('Location:fail.php') ;
 	
 	$conn->close();
 	
