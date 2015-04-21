14412d4a49fc0229f57c48212c965b14e52f3bc3
eoyarzun <eoyarzun6668@gmail.com>
2015-04-21 16:36:08 -0400

    Use create-recipe-form.php to delete recipes and cookbooks

--- delete.php
+++ delete.php
@@ -1,66 +1,29 @@
 <?php
-session_start() ;
-include 'db-credentials.php' ;
+	session_start() ;
+	include 'db-credentials.php' ;
+	include 'create-recipe-form.php' ;
 
-if(isset($_GET["recipe_id"]))
-	deleteRecipe() ;
-else 
-	deleteCookbook() ;
-
-function deleteRecipe() 
-{
-	$recipe_id = $_GET["recipe_id"] ;
-	
 	$conn = new mysqli($servername, $username, $password, $dbname);
-	
-	$sql = " DELETE FROM recipe 
-				WHERE recipe_id = '$recipe_id'" ;
-	
-	if ($conn->query($sql) !== TRUE) header('Location:fail.php') ;
-
-	$sql = " DELETE FROM tag 
-				WHERE type = 'RECIPE'
-					AND type_id = '$recipe_id'" ;
-
-	if ($conn->query($sql) !== TRUE) header('Location:fail.php') ;
-	
-	
-	$sql = " DELETE FROM friends 
-				WHERE type = 'RECIPE'
-					AND type_id = '$recipe_id'" ;
-
-	if ($conn->query($sql) !== TRUE) header('Location:fail.php') ;
-	
-	
-	$conn->close();
-	header('Location:my-recipes.php') ;
-}	
-
-function deleteCookbook() 
-{
-	$cookbook_id = $_GET["cookbook_id"] ;
-	
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	
-	$sql = " DELETE FROM cookbook 
-				WHERE cookbook_id = '$cookbook_id'" ;
-
-	if ($conn->query($sql) !== TRUE) header('Location:fail.php') ;
-	
-	$sql = " DELETE FROM tag 
-				WHERE type = 'COOKBOOK'
-					AND type_id = '$cookbook_id'" ;
-
-	if ($conn->query($sql) !== TRUE) header('Location:fail.php') ;
-	
-	$sql = " DELETE FROM friends 
-				WHERE type = 'COOKBOOK'
-					AND type_id = '$cookbook_id'" ;
-
-	if ($conn->query($sql) !== TRUE)  header('Location:fail.php') ;
-	
-	$conn->close();
-	
-	header('Location:my-cookbooks.php') ;
-}	
+		
+	if ($conn->connect_error) 
+	{
+		die("Connection failed: " . $conn->connect_error);
+	}
+		
+	if(isset($_GET["recipe_id"]))
+	{
+		$recipe_id = $_GET["recipe_id"] ;
+		removeRecipe($conn, $recipe_id);
+		header('Location: my-recipes.php');
+	}
+	else 
+	{
+		$id = $_GET["cookbook_id"] ;
+		removeFromDbType("Friends", "type_id", $id, $conn);
+		removeFromDbType("Tag", "type_id", $id, $conn);
+		removeFromDb("cookbook_list", "cookbook_id", $id, $conn);
+		removeFromDb("Recipe_list", "recipe_id", $id, $conn);
+		removeFromDb("cookbook", "cookbook_id", $id, $conn);
+		header('Location: my-cookbooks.php');
+	}
 ?>
